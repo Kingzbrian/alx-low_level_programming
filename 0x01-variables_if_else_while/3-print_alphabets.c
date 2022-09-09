2b927fa5a705751b872680d318e507b01e5ea294
Kingzbrian <murageb254@gmail.com>
2022-09-09 02:48:37 -0700

    revise

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry
  * Return: End the program
  */
 int main(void)
 {
 	char alpha = 'a';
+
 	char ALPHA = 'A';
+
 	while (alpha <= 'z')
 	{
 		putchar(alpha);
