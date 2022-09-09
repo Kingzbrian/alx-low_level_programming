d5dc48205462353c977b3ab9de3f8584672483c5
Kingzbrian <murageb254@gmail.com>
2022-09-09 02:54:30 -0700

    revise

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
+/**
  * main - Entry
  * Return: end the program
  */
 int main(void)
 {
 	char alpha = 'z';
+
 	while (alpha >= 'a')
 	{
 		putchar(alpha);
