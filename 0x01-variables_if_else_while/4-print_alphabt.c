b9a996c5c666fb34e1205f9efd88f290b7d645a5
Kingzbrian <murageb254@gmail.com>
2022-09-09 02:49:34 -0700

    revise

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry
  * Return: end the program
  */
 int main(void)
 {
 	char alpha = 'a';
+
 	while (alpha <= 'z')
 	{
 		if (alpha != 'q' && alpha != 'e')
