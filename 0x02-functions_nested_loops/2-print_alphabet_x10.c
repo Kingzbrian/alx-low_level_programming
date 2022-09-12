954f106a1666530f17156f252dffa7c2cedfeb3e
Kingzbrian <murageb254@gmail.com>
2022-09-12 01:45:13 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10(void) - print alphabet 10 times
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int n = 1;
+
+	while(n <= 10)
+	{
+		char al = 'a';
+		while(al <= 'z')
+		{
+			_putchar(al);
+			al++;
+		}
+		n++;
+		_putchar('\n');
+	}
+}
