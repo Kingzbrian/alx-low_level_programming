075bc53b52036d35ef9581d445e9f7c220bf43b6
Kingzbrian <murageb254@gmail.com>
2022-09-13 05:58:24 -0700

    fin fib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - start
+ *Return: end
+ */
+int main(void)
+{
+	long int a = 0, b = 1, d;
+	int c;
+	for (c = 0; c < 98; c++)
+	{
+		d = a + b;
+		a = b;
+		b = d;
+		printf("%ld", d);
+		if (c < 97)
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
