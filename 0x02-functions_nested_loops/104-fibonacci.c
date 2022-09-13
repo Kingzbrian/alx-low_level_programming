eacec318ad781854801617f4df7d31cbde040432
Kingzbrian <murageb254@gmail.com>
2022-09-13 06:33:17 -0700

    fin fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,19 +5,28 @@
  */
 int main(void)
 {
-	unsigned long int a = 0, b = 1, d;
+	unsigned long int j = 1, k = 2, j1, j2, k1, k2;
 	int c;
 
-	for (c = 0; c < 98; c++)
+	printf("%lu", j);
+	for (c = 0; c < 91; c++)
 	{
-		d = a + b;
-		a = b;
-		b = d;
-		printf("%lu", d);
-		if (c < 97)
-		{
-			printf(", ");
-		}
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
+	}
+		j1 = j / 1000000000;
+		j2 = j % 1000000000;
+		k1 = k / 1000000000;
+		k2 = k % 1000000000;
+	for (c = 92; c < 99; c++)
+	{
+		printf(", %lu", k1 + k2 / 1000000000);
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
 	putchar('\n');
 	return (0);
