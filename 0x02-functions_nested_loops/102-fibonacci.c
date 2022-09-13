783fa0e3c256918d59a477f9c1aec1d2d2972379
Kingzbrian <murageb254@gmail.com>
2022-09-13 04:03:52 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,15 +8,15 @@ int main(void)
 	long int a = 0, b = 1, d;
 	int c;
 	
-	printf("%ld", b);
 	for (c = 0; c < 50; c++)
 	{
 		d = a + b;
 		a = b;
 		b = d;
-		if (d != 1)
+		printf("%ld", d);
+		if (c < 49)
 		{
-			printf(", %ld", d);
+			printf(", ");
 		}
 	}
 	return (0);
