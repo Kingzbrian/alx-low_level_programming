cb3a80e39e6bb96f0d9bbaa38bef5bfb743dae6c
Kingzbrian <murageb254@gmail.com>
2022-09-14 00:07:36 -0700

    postive negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests funct that prints if integer is postive or negative
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
