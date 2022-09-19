ea09012f2dbd85ee87f7decf9c6622297a808493
Kingzbrian <murageb254@gmail.com>
2022-09-19 04:00:49 -0700

    coppy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcpy - copy one string to annother
+ * @dest: destunation
+ * @src: source
+ * Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = src;
+	dest = i;
+	return (dest);
+}
