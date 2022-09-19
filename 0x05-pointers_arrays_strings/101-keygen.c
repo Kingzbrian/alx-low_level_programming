0bc5c6abd46c6003fafd4f0701c0e4ac50b538b7
Kingzbrian <murageb254@gmail.com>
2022-09-19 04:33:53 -0700

    random pass

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+	int randpass;
+	int count = 0;
+	int tt = 2772;
+
+	srand(time(NULL));
+	while (tt > 122)
+	{
+		randpass = (rand() % 125) + 1;
+		printf("%c", randpass);
+		tt -= randpass;
+	}
+	printf("%c", total);
+	return (0);
+}
