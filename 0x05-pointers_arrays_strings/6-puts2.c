9b6b2907c4625bbcdb5d01d7850840765541f1c9
Kingzbrian <murageb254@gmail.com>
2022-09-19 02:40:45 -0700

    characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - prints every xter of a string in a new line
+ * @str: pointr\er to string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	while (s[i])
+	{
+		_putchar(i'\n');
+		i++
+	}
+}
