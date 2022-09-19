d23f5ffc919eb35bf1173647fb197ed7983de916
Kingzbrian <murageb254@gmail.com>
2022-09-19 02:22:17 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: pointer
+ * Return: reversed string
+ */
+void rev_string(char *s)
+{
+	int i;
+	int rev;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	rev = i;
+	while (i--)
+	{
+		return (rev);
+	}
+}
