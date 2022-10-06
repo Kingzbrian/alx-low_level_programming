0eb17676da3a781b16508165aa59bc71fbba6b74
Kingzbrian <murageb254@gmail.com>
2022-10-06 06:26:09 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,25 +1,24 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc - function to return pointer to allocated mem
- * @nmemb: no of elements
- * @size: no of bytes
+ * _calloc - allocates memory
+ * @nmemb: number of elements.
+ * @size: size of bytes
  * Return: pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *c;
-	unsigned int *c;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
-	c = malloc(nmemb * size);
-	if (c == NULL)
+	}
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		;
 	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
-	return (c);
+	return (p);
 }
