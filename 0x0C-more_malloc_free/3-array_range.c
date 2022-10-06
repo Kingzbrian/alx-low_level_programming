37479bf780a5eca489e2948b669436cc9ea2d29d
Kingzbrian <murageb254@gmail.com>
2022-10-06 06:48:44 -0700

    create array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * simple_print_buffer - prints buffer
+ * @buffer: the address
+ * @size: the size
+ * Return: null or pointer
+ */
+int *array_range(int min, int max)
+{
+	char *c;
+	int j;
+
+	if (min > max)
+		return (NULL);
+	c = malloc(sizeof(int) * (min - max + 1);
+	if (c == NULL)
+		return (NULL);
+	for (i = 0; min <= max; i++, min++)
+		c[j] = min;
+	return (c);
+}
