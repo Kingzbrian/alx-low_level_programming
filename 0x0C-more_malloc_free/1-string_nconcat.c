cf477a343e61484e0d483090d9dbd49b35f74a7c
Kingzbrian <murageb254@gmail.com>
2022-10-06 04:44:53 -0700

    cat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,30 +8,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *c;
-	unsigned int i, j, k, l;
+	char *a;
+	unsigned int x, n1 = 0, n2 = 0, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[n1] != '\0'; n1++)
 		;
-	for (j = 0; s1[j] != '\0'; j++)
+	for (j = 0; s1[n2] != '\0'; n2++)
 		;
-	if (n > j)
-		n = j;
-	k = i + n;
-	c = malloc(sizeof(k + 1));
-	if (c == NULL)
+	if (n < n2)
+		x = n;
+	else
+		x = n2;
+	a = malloc(sizeof(char) * (n1 + x + 1));
+	if (a == NULL)
 		return (NULL);
-	for (l = 0; l < k; l++)
+	for (i = 0; i < n1; i++)
 	{
-		if (l < i)
-			c[l] = s1[l];
-		else
-			c[l] = s2[l - i];
+		a[i] = s1[i];
 	}
-	c[l] = '\0';
-	return (c);
+	for (j = 0; j < x; j++)
+	{
+		a[i + j] = s2[j];
+	}
+	a[i + j] = 0;
+	return (a);
 }
