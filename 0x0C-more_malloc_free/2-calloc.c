52e53f1804e4edfe1bbdd85de7e7ae251ce9b6de
Kingzbrian <murageb254@gmail.com>
2022-10-06 06:10:28 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,11 +4,11 @@
  * _calloc - function to return pointer to allocated mem
  * @nmemb: no of elements
  * @size: no of bytes
- * Return: 0
+ * Return: pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *c;
+	char *c;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -16,24 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	c = malloc(nmemb * size);
 	if (c == NULL)
 		return (NULL);
-	_memset(c, 0, nmemb * size);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 	return (c);
-}
-/**
- * _memset - fills mem with a nstant byte
- * @s: return mem
- * @b: constant byte
- * @n: size of bytes
- * Return: character
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsugned int i = 0;
-
-	while (i < n)
-	{
-		s[i] = b;
-		i += 1;
-	}
-	return (s);
 }
