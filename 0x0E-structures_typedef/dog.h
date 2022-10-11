cb67b605c215996bec4759ab9cbd01de1ffed366
Kingzbrian <murageb254@gmail.com>
2022-10-11 05:25:32 -0700

    dog stuct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - this is a sructure called dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: A simple dog structure containing its name, age and owner
+ */
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
